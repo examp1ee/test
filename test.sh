af3d814365d7e326e57a1ff08214f865cf166bef
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-20 14:21:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # Options
-. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/colors.sh) --
+#. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
 	case "$1" in
