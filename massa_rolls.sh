8125a39f61864bf421dabd4bb4323e50ddc8bffa
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-20 16:36:40 +0200

    Update massa_rolls.sh

--- massa_rolls.sh
+++ massa_rolls.sh
@@ -27,16 +27,6 @@ echo -e "\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\
 echo -e ""
 
 echo -e "\033[0m"
-#echo
-#echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
-#echo
-#echo -e "${C_LGn}Options${RES}:"
-#echo -e "  -h, --help       show the help page"
-#echo -e "  -n, --node PORT  assign the specified port to use RPC (default is ${C_LGn}${node}${RES})"
-#echo -e "  -r, --rpc PORT   assign the specified port to use RPC (default is ${C_LGn}${rpc}${RES})"
-#echo -e "  -u, --update     update the node"
-#echo
-#echo
 
 cd /root
 
