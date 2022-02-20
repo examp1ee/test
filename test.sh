e6d4cbfb73ce27986a1a2473139ca069b56efe70
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-20 15:23:01 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -38,3 +38,38 @@ echo -e "\033[0m"
 #echo
 #echo
 
+cd /root
+
+sleep 5
+
+sudo tee /root/rollsup.sh > /dev/null <<EOF 
+cd /root/massa/massa-client
+
+candidat=\$(./massa-client wallet_info |grep 'Candidate rolls'|awk '{print \$3}')
+massa_wallet_address=\$(./massa-client wallet_info |grep 'Address'|awk '{print \$2}')
+tmp_final_balans=\$(./massa-client wallet_info |grep 'Final balance'|awk '{print \$3}')
+final_balans=\${tmp_final_balans%%.*}
+averagetmp=\$(/usr/bin/cat /proc/loadavg | awk '{print \$1}')
+kv=100
+var=\$(echo "scale=0; \$averagetmp*\$kv" | bc)
+var1=\${var%%.*}
+if [ \$var1 -lt "20" ];then
+echo \`/bin/date +"%b %d %H:%M"\` "(rollsup) Загрузка машины \$var1% Нода скорее всего в подвисшем состоянии rolls покупаться не будет" >> /root/rolls.log
+elif [ -z "\$candidat" ];then
+echo \`/bin/date +"%b %d %H:%M"\` "Нода в данный момент не в сети" >> /root/rolls.log
+elif [ \$candidat -gt "0" ];then
+echo "Ok" > /dev/null
+elif [ \$final_balans -gt "99" ]; then
+echo \`/bin/date +"%b %d %H:%M"\` "Ролл слетел, проверяем количество монеток и пробуем купить" >> /root/rolls.log
+else
+echo \`/bin/date +"%b %d %H:%M"\` "Недостаточно монет для покупки ролла у вас \$final_balans, необходимо минимум 100" >> /root/rolls.log
+fi
+EOF
+
+sleep 5
+
+printf "SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/3 * * * * root /bin/bash /root/rollsup.sh > /dev/null 2>&1
+" > /etc/cron.d/massarolls
+
