c881dc4812789b5cb20103c4c0535dc651afd0f7
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-21 17:02:38 +0200

    Update massa_rolls.sh

--- massa_rolls.sh
+++ massa_rolls.sh
@@ -29,15 +29,33 @@ echo -e ""
 echo -e "\033[0m"
 
 
-#rm -f $HOME/massa/massa-client/massa-client
-#if [ ! -e $HOME/massa/massa-client/massa-client ]; then
-#  wget https://raw.githubusercontent.com/I-Example-I/test/main/massa-client -O $HOME/massa/massa-client/massa-client
-#  chmod +x $HOME/massa/massa-client/massa-client
-#fi
+rm -f $HOME/massa/massa-client/massa-client
+if [ ! -e $HOME/massa/massa-client/massa-client ]; then
+  wget https://raw.githubusercontent.com/I-Example-I/test/main/massa-client -O $HOME/massa/massa-client/massa-client
+  chmod +x $HOME/massa/massa-client/massa-client
+fi
 
 
 cd $HOME/massa/massa-client
 massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
-echo $massa_wallet_address;
-balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
-echo $balance;
+#echo $massa_wallet_address;
+while true
+do
+        balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        int_balance=${balance%%.*}
+        if [ $int_balance -gt "99" ]; then
+                echo "More than 99"
+                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                echo $resp
+        elif [ $int_balance -lt "100" ]; then
+                echo "Less than 100"
+        fi
+    
+        printf "sleep"
+        for((sec=0; sec<60; sec++))
+        do
+                printf "..."
+                sleep 1
+        done
+        printf "\n"
+done
