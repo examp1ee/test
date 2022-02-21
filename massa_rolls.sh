e4f7e0a393067ce125777f335707ed297c3e497b
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-21 16:42:14 +0200

    Update massa_rolls.sh

--- massa_rolls.sh
+++ massa_rolls.sh
@@ -28,38 +28,14 @@ echo -e ""
 
 echo -e "\033[0m"
 
-cd /root
 
-sleep 5
+#rm -f $HOME/massa/massa-client/massa-client
+#if [ ! -e $HOME/massa/massa-client/massa-client ]; then
+#  wget https://raw.githubusercontent.com/I-Example-I/test/main/massa-client -O $HOME/massa/massa-client/massa-client
+#  chmod +x $HOME/massa/massa-client/massa-client
+#fi
 
-sudo tee /root/rollsup.sh > /dev/null <<EOF 
-cd /root/massa/massa-client
 
-candidat=\$(./massa-client wallet_info |grep 'Candidate rolls'|awk '{print \$3}')
-massa_wallet_address=\$(./massa-client wallet_info |grep 'Address'|awk '{print \$2}')
-tmp_final_balans=\$(./massa-client wallet_info |grep 'Final balance'|awk '{print \$3}')
-final_balans=\${tmp_final_balans%%.*}
-averagetmp=\$(/usr/bin/cat /proc/loadavg | awk '{print \$1}')
-kv=100
-var=\$(echo "scale=0; \$averagetmp*\$kv" | bc)
-var1=\${var%%.*}
-if [ \$var1 -lt "20" ];then
-echo \`/bin/date +"%b %d %H:%M"\` "(rollsup) Завантаження машини \$var1% Швидше за все нода зависла, rolls купуватися не буде" >> /root/rolls.log
-elif [ -z "\$candidat" ];then
-echo \`/bin/date +"%b %d %H:%M"\` "Зараз нода не в мережі" >> /root/rolls.log
-elif [ \$candidat -gt "0" ];then
-echo "Ok" > /dev/null
-elif [ \$final_balans -gt "99" ]; then
-echo \`/bin/date +"%b %d %H:%M"\` "Ролл злетів, перевіряємо кількість монеток і пробуємо купити" >> /root/rolls.log
-else
-echo \`/bin/date +"%b %d %H:%M"\` "Недостатньо монет для покупки ролла, у вас \$final_balans, необхідно мінімум 100" >> /root/rolls.log
-fi
-EOF
-
-sleep 5
-
-printf "SHELL=/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/3 * * * * root /bin/bash /root/rollsup.sh > /dev/null 2>&1
-" > /etc/cron.d/massarolls
-
+cd $HOME/massa/massa-client
+massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+echo $massa_wallet_address;
