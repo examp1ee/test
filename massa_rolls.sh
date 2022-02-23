54cf56d24e074bdfd8d7672fe3d563d08423f571
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-02-23 18:04:30 +0200

    Update massa_rolls.sh

--- massa_rolls.sh
+++ massa_rolls.sh
@@ -55,11 +55,11 @@ do
         #int_balance=${balance%%.*}
         if [ $int_balance -gt "99" ]; then
                 echo "More than 99"
-                sudo systemctl restart massad
-                sleep 100
                 #resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
                 resp=$(./massa-client buy_rolls $massa_wallet_address 1 0)
+                echo "massa_client"
                 echo $resp
+                echo "exit"
         elif [ $int_balance -lt "100" ]; then
                 echo "Less than 100"
         fi
